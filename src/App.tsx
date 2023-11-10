406b2da681736224bd5879045c05a9193c0fa2bb
unknown <ulanuulunursultan36@gmail.com>
2023-11-10 16:19:06 +0600

    i fineshed modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import Modal from './components/modal/Modal';
+
 function App() {
-  return <div />;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+  return (
+    <div>
+      <button onClick={handleOpen}>открыть</button>
+      <Modal
+        isOpen={modalOpen}
+        onClose={handleClose}
+        borderClass="rounded"
+        width=""
+        height=""
+      >
+        <p>Hello world!</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam,
+          doloremque quo consequatur ratione facilis vel voluptas.
+          Reprehenderit, dolores laborum! Vitae, nobis. Quod, temporibus animi
+          ullam dignissimos rem ipsa voluptates iusto?
+        </p>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+interface ModalProps {
+  isOpen: boolean;
+  children: React.ReactNode;
+  onClose: () => void;
+  borderClass?: string;
+  width: string;
+  height: string;
+}
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  children,
+  onClose,
+  borderClass,
+  width,
+  height,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal modal-open">
+      <div
+        className={`modal-box fixed rounded ${borderClass} ${width} ${height}`}
+      >
+        <span
+          className="cursor-pointer	absolute top-0 right-2 "
+          onClick={onClose}
+        >
+          x
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
