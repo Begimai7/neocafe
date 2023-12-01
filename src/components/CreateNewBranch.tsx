757a89e9cea3ddd0fde480bd370211c68bc383c4
kanykej <kanykej.ka.777.@gmail.com>
2023-12-01 14:31:07 +0600

    fixed new branch modal

--- src/components/CreateNewBranch.tsx
+++ src/components/CreateNewBranch.tsx
@@ -1,13 +1,14 @@
 import Modal from './UI/Modal';
 import { useState } from 'react';
 import Input from './UI/Input';
-import image from '../assets/newBranchIcons/cloudArrowUp.svg';
 import closeIcon from '../assets/newBranchIcons/close.svg';
+import Button from './UI/Button';
+// import PhotoInput from './UI/PhotoInput';8
 
 const NewBranch: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
-
   const handleClose = () => setIsOpen(false);
+
   return (
     <Modal
       width="600px"
@@ -16,149 +17,73 @@ const NewBranch: React.FC = () => {
       borderRadius="30px"
       padding="32px"
     >
-      <div className="bg-red-500">
-        <div
-          className="flex items-center content-between bg-red-500"
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <h5 className="font-bold" style={{ fontSize: '24px' }}>
-            Новый филиал
-          </h5>
-          <img style={{marginBottom: '9px'}} src={closeIcon} />
-        </div>
-        <p
-          className="font-semibold"
-          style={{ marginTop: '24px', fontSize: '22px' }}
-        >
-          Добавьте фотографию филиала
-        </p>
+      <div className="flex items-center justify-between">
+        <h5 className="font-bold text-[24px]">Новый филиал</h5>
+        <img className="mb-[9px]" src={closeIcon} />
+      </div>
+      <p className="font-semibold mt-[24px] text-[22px]">
+        Добавьте фотографию филиала
+      </p>
 
-        <label
-          htmlFor="new_branch"
-        >
-          <div
-            className="flex justify-center items-center w-full bg-orange-700"
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: '#F4F4F4',
-              height: '209px',
-              marginTop: '16px',
-            }}
-          >
-            <div
-              className="flex justify-center items-center"
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                width: 'calc(100% - 32px)',
-                height: 'calc(100% - 36px)',
-                backgroundColor: '#FFFFFF',
-                border: '1px dashed #CDD3DD',
-              }}
-            >
-              <img src={image} alt="Uploaded Image" />
-              <p
-                className="text-center m-w-[309px]"
-                style={{ textAlign: 'center', maxWidth: '309px' }}
-              >
-                Перетащите изображение для изменения или обзор
-              </p>
-            </div>
-          </div>
-        </label>
-        <Input id="new_branch" style={{ display: 'none' }} type="file" />
+      {/* <PhotoInput /> */}
 
-        <p
+      <p className="text-[22px] font-semibold uppercase mt-6">
+        Название и адрес
+      </p>
+      <div className="mt-4">
+        <p className="text-[#C1C1C3] mb-2">Название кофейни</p>
+        <Input
+          id="29"
+          name="name of branch"
+          placeholder="Название филиала"
+          type="text"
+          // label="Название кофейни"
           style={{
-            fontSize: '22px',
-            fontWeight: '600',
-            textTransform: 'uppercase',
-            marginTop: '24px',
+            padding: '16px 18px',
+            backgroundColor: '#EDEDED',
+            width: '100%',
+            borderRadius: '10px',
           }}
-        >
-          Название и адрес
-        </p>
-        <div
+        />
+      </div>
+      <div>
+        <p className="text-[#C1C1C3] mb-2">Адрес</p>
+        <Input
+          id="29"
+          name="adress of branch"
+          placeholder="Адрес нового филиала"
+          type="text"
           style={{
-            marginTop: '16px',
+            padding: '16px 18px',
+            backgroundColor: '#EDEDED',
+            width: '100%',
+            borderRadius: '10px',
           }}
-        >
-          <p
-            style={{
-              color: '#C1C1C3',
-              marginBottom: '8px',
-            }}
-          >
-            
-            Название кофейни
-          </p>
-          <Input
-            id="29"
-            name="name of branch"
-            placeholder="Название филиала"
-            type="text"
-            // label="Название кофейни"
-            style={{
-              padding: '16px 18px',
-              backgroundColor: '#EDEDED',
-              width: '100%',
-              borderRadius: '10px',
-            }}
-          />
-        </div>
-        <div>
-          <p
-            style={{
-              color: '#C1C1C3',
-              marginBottom: '8px',
-            }}
-          >
-            Адрес
-          </p>
-          <Input
-            id="29"
-            name="adress of branch"
-            placeholder="Адрес нового филиала"
-            type="text"
-            style={{
-              padding: '16px 18px',
-              backgroundColor: '#EDEDED',
-              width: '100%',
-              borderRadius: '10px',
-            }}
-          />
-        </div>
-        <div>
-          <p
-            style={{
-              color: '#C1C1C3',
-              marginBottom: '8px',
-            }}
-          >
-            Номер телефона
-          </p>
-          <Input
-            id="29"
-            name="phone number of branch"
-            placeholder="Введите номер телефона"
-            type="tel"
-            style={{
-              padding: '16px 18px',
-              backgroundColor: '#EDEDED',
-              width: '100%',
-              borderRadius: '10px',
-            }}
-          />
-        </div>
-        <p>Заполните график работы</p>
+        />
+      </div>
+      <div>
+        <p className="text-[#C1C1C3] mb-2">Номер телефона</p>
+        <Input
+          id="29"
+          name="phone number of branch"
+          placeholder="Введите номер телефона"
+          type="tel"
+          style={{
+            padding: '16px 18px',
+            backgroundColor: '#EDEDED',
+            width: '100%',
+            borderRadius: '10px',
+          }}
+        />
+      </div>
+      <p>Заполните график работы</p>
+      <div className="flex gap-6 mt-12">
+        <Button styles="border border-[#00315D] py-4 w-[100%] rounded-md type='submit' text-[#00315D] font-semibold">
+          Отмена
+        </Button>
+        <Button styles="bg-[#00315D] py-4 w-[100%] rounded-md type='submit' text-white font-semibold">
+          Сохранить
+        </Button>
       </div>
     </Modal>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ import daisyui from 'daisyui';
 
 / @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src//*.{js,ts,jsx,tsx}'],
+  // content: ['./index.html', './src//*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/components/Toastify.tsx",
+    "./src/components/CreateNewBranch.tsx",
+    "./src/components/UI/PhotoInput.tsx"
+
+  ],
   theme: {
     extend: {},
   },
