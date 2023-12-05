5e96ab13425ccdf20da264b9995ac50f2cf5eea8
unknown <ulanuulunursultan36@gmail.com>
2023-12-05 12:40:33 +0600

    gchjhvljh

--- src/components/signIn/SignIn.tsx
+++ src/components/signIn/SignIn.tsx
@@ -1,67 +1,86 @@
 import React from 'react';
 import Modal from '../UI/Modal';
-import img from '../../assets/adminSignIn/admin.png'
+import img from '../../assets/adminSignIn/admin.png';
 import Button from '../UI/Button';
 import PassportInput from '../UI/PassportInput';
+import styled from 'styled-components';
 const SignIn = () => {
   const [isOpen, setIsOpen] = React.useState(true);
   const handleClose = () => setIsOpen(true);
 
   return (
-    <div>
-      <div
-        className="bg-cover bg-center w-full h-screen"
-        style={{
-          backgroundImage: `url(${img})`,
-        }}
+    <SignInStyled>
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        width="auto"
+        height="auto"
+        borderRadius="20px"
       >
-        <Modal
-          open={isOpen}
-          onClose={handleClose}
-          width="auto"
-          height="auto"
-          borderRadius="20px"
-        >
-          <div className="p-[40px]">
-            <div className="text-center">
-              <h1 className="text-black text-7xl font-bold ">Вход</h1>
-            </div>
-            <form className="">
-              <div className="mb-4 text-center mt-[32px]">
-                <label
-                  htmlFor="phone-number"
-                  className="text-lg font-semibold "
-                >
-                  {/* Номер телефона */}
-                  <input
-                    type="text"
-                    id="phone-number"
-                    className="block rounded-xl py-[22px] mt-[48px] text-center outline-none text-black text-xl font-semibold"
-                    placeholder="Логин"
-                    required
-                    style={{ backgroundColor: '#EDEDED', width: '100%' }}
-                  />
-                </label>
-                <PassportInput
-                  placeholder="Пароль"
-                  id="passport-input"
-                  style={{
-                    width: '534px',
-                    background: '#EDEDED',
-                    borderRadius: '10px',
-                    padding: '22px',
-                    outline: 'none',
-                    marginTop: '32px',
-                  }}
-                />
-              </div>
-              <Button styles={'w-full bg-[#5C7994] border mt-[33px] py-[19px]'}>Войти</Button>
-            </form>
-          </div>
-        </Modal>
-      </div>
-    </div>
+        <Box>
+          <TextCenter>
+            <Text className="">Вход</Text>
+          </TextCenter>
+          <form>
+            <InputBox>
+              <label htmlFor="phone-number" className="text-lg font-semibold ">
+                {/* Номер телефона */}
+              </label>
+              <input
+                type="text"
+                id="phone-number"
+                className="block rounded-xl py-[22px] mt-[48px] text-center outline-none text-black text-xl font-semibold"
+                placeholder="Логин"
+                required
+                style={{ backgroundColor: '#EDEDED', width: '100%' }}
+              />
+              <PassportInput
+                placeholder="Пароль"
+                id="passport-input"
+                style={{
+                  width: '534px',
+                  background: '#EDEDED',
+                  borderRadius: '10px',
+                  padding: '22px',
+                  outline: 'none',
+                  marginTop: '32px',
+                }}
+              />
+            </InputBox>
+            <Button styles={'w-full bg-[#5C7994] border mt-[33px] py-[19px]'}>
+              Войти
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+    </SignInStyled>
   );
 };
 
 export default SignIn;
+
+const SignInStyled = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Box = styled.div`
+  padding: 0 40px 40px;
+`;
+
+const TextCenter = styled.div`
+  text-align: center;
+`;
+
+const Text = styled.h1`
+  color: black;
+  font-weight: 700;
+  font-size: 72px;
+`
+
+const InputBox = styled.div`
+  margin-bottom: 16px;
+`
