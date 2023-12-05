2648d162c50f927a154008d1724fa865e5fae578
unknown <ulanuulunursultan36@gmail.com>
2023-12-05 13:28:45 +0600

    lbjhsffvkjbkjb

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import {ToastContainer} from 'react-'
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
